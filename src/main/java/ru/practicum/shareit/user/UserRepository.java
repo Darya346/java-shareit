04ffe51ea9f4ef678928e300b5e70ef765c15ab6
Darya346 <dashkins346@gmail.com>
2026-03-11 22:05:31 +0300

    fix: resolve checkstyle empty line issues in interfaces

--- src/main/java/ru/practicum/shareit/user/UserRepository.java
+++ src/main/java/ru/practicum/shareit/user/UserRepository.java
@@ -5,11 +5,18 @@ import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository {
+
     User save(User user);
+
     User update(User user);
+
     Optional<User> findById(Long id);
+
     List<User> findAll();
+
     void deleteById(Long id);
+
     boolean existsByEmail(String email);
+
     boolean existsByEmailAndNotId(String email, Long id);
 }
